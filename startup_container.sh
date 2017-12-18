9598e081a057f09f8bd0e093bb73e08e7a9d32a0
Salim Hermidas <salim@hermidas.ch>
2017-12-18 10:31:08 +0100

    Replace mail config from env vars

--- startup_container.sh
+++ startup_container.sh
@@ -8,6 +8,12 @@ git init
 git remote add origin https://github.com/ASE-thingy-blue/thingy-api-blue.git
 git pull origin master
 
+# Configure Mailer
+sed -i "s/from: \"{MAILFROM}\"/from: \"${MAILFROM}\"/g" backend/mailer.js
+sed -i "s/host: \"{MAILHOST}\"/host: \"${MAILHOST}\"/g" backend/mailer.js
+sed -i "s/user: \"{MAILUSER}\"/user: \"${MAILUSER}\"/g" backend/mailer.js
+sed -i "s/pass: \"{MAILPASS}\"/pass: \"${MAILPASS}\"/g" backend/mailer.js
+
 # Install all dependencies
 npm install
 
