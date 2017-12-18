15628f9fb31b93632580cfc077450aa661e83c9c
Salim Hermidas <salim@hermidas.ch>
2017-12-18 11:02:09 +0100

    Fix startup script

--- startup_container.sh
+++ startup_container.sh
@@ -9,10 +9,10 @@ git remote add origin https://github.com/ASE-thingy-blue/thingy-api-blue.git
 git pull origin master
 
 # Configure Mailer
-sed -i "s/from: \"{MAILFROM}\"/from: \"${MAILFROM}\"/g" backend/mailer.js
-sed -i "s/host: \"{MAILHOST}\"/host: \"${MAILHOST}\"/g" backend/mailer.js
-sed -i "s/user: \"{MAILUSER}\"/user: \"${MAILUSER}\"/g" backend/mailer.js
-sed -i "s/pass: \"{MAILPASS}\"/pass: \"${MAILPASS}\"/g" backend/mailer.js
+sed -i "s/from: \"{MAILFROM}\"/from: ${MAILFROM}/g" backend/mailer.js
+sed -i "s/host: \"{MAILHOST}\"/host: ${MAILHOST}/g" backend/mailer.js
+sed -i "s/user: \"{MAILUSER}\"/user: ${MAILUSER}/g" backend/mailer.js
+sed -i "s/pass: \"{MAILPASS}\"/pass: ${MAILPASS}/g" backend/mailer.js
 
 # Install all dependencies
 npm install
